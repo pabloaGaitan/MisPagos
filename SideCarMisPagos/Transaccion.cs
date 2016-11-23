e5c7de58231384817728e78540aee9fd1c49a987
Miguel <cmiguelmg@gmail.com>
2016-11-22 20:21:14 -0500

    mis pagos

---
+++ SideCarMisPagos/Transaccion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideCarMisPagos
+{
+    class Transaccion
+    {
+        public string TransaccionId { get; set; }
+        public string fecha { get; set; }
+        public Usuario Cedula_usuario { get; set; }
+        public double valor { get; set; }
+
+        public Transaccion() { }
+    }
+}
---
+++ SideCarMisPagos/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SideCarMisPagos
+{
+    class Usuario
+    {
+        public string UsuarioId { get; set; }
+        public string UsuarioMisPagos { get; set; }
+        public string Contrasenia { get; set; }
+        public double saldo { get; set; }
+        public Usuario() { }
+
+    }
+}
